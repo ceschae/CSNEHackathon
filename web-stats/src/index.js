f86e51d1014f3746aa5f2fc8fe33e43d1d77c073
Caitlin Schaefer <ceschae@cs.washington.edu>
2018-04-08 20:21:23 -0700

    adding firebase hooks

--- web-stats/src/index.js
+++ web-stats/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import constants from './components/constants'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+var config = {
+    apiKey: constants.apiKey,
+    authDomain: constants.authDomain,
+    databaseURL: constants.databaseURL,
+    projectId: constants.projectId,
+    storageBucket: constants.storageBucket,
+    messagingSenderId: constants.messagingSenderId
+  };
+  firebase.initializeApp(config);
