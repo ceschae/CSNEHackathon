ac74b594e06508f6f447a5bb91d7f3af001041fb
Caitlin Schaefer <ceschae@cs.washington.edu>
2018-04-08 20:54:32 -0700

    working and deployed web app!

--- web-stats/src/App.js
+++ web-stats/src/App.js
@@ -6,22 +6,48 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            err: ""
+        }
+    }
 
+    render() {
 
+        firebase.database().ref("word-stats")
+            .once("value")
+            .then(snapShot => {
+                this.setState({wordsRef: snapShot});
+            });
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+        if(!this.state.wordsRef) {
+            return <div>Loading... Please be patient</div>;
+        }
+
+        let words = [];
+        this.state.wordsRef.forEach(wordSnapshot => {
+            (wordSnapshot.val().average < 0.8) ?
+            words.push(
+                <li class="list-group-item"><b>{wordSnapshot.val().word}</b>: {wordSnapshot.val().average}</li>
+            )
+            : undefined
+        });
+
+        return (
+            <div className="App">
+                <div class="jumbotron">
+                    <h1 class="display-4">Voicebox</h1>
+                    <p class="lead">Speech Within Reach</p>
+                    <hr class="my-4" />
+                    <img src="http://www.csne-erc.org/sites/default/files/NSF-CSNE-LOGO3.png" alt="CSNE" />
+                </div>
+                <ul class="list-group">
+                    {words}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
--- web-stats/src/index.js
+++ web-stats/src/index.js
@@ -5,15 +5,18 @@ import App from './App';
 import constants from './components/constants'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 var config = {
-    apiKey: constants.apiKey,
-    authDomain: constants.authDomain,
-    databaseURL: constants.databaseURL,
-    projectId: constants.projectId,
-    storageBucket: constants.storageBucket,
-    messagingSenderId: constants.messagingSenderId
+    apiKey: constants.firebase.apiKey,
+    authDomain: constants.firebase.authDomain,
+    databaseURL: constants.firebase.databaseURL,
+    projectId: constants.firebase.projectId,
+    storageBucket: constants.firebase.storageBucket,
+    messagingSenderId: constants.firebase.messagingSenderId
   };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
